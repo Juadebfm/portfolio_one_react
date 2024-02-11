131a696f15bd09df9c572d36e0eb22985e9dd856
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-02-11 13:39:24 -0800

    FEAT: Setting up the project files

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Navbar from './components/Navbar';
+
+export default function App() {
+  return (
+    <div className='font-inter bg-[#050507] h-[100%] w-[100%] text-white'>
+      <Navbar />
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary_green: '#079211',
+      },
+    },
+  },
+  plugins: [],
+};
