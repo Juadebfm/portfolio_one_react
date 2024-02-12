3a8c52c376d07e9e4e6b3316de13419c2ca1360d
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-02-11 19:01:41 -0800

    FEAT: Done

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "../index.css";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        const posts = await response.json();
+        console.log(posts);
+        setData(posts);
+      };
+      fetchData();
+    } catch (error) {
+      console.log("Error Discovered:", error);
+    }
+  }, []);
+
+  return (
+    <main className="container_pd Blog_page">
+      {data.map((post) => (
+        <div key={post.id} className="Blog_news_card">
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </main>
+  );
 };
 
 export default Blog;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Services from "../components/Services";
 
 const Homepage = () => {
   return (
     <main>
       <Hero />
+      <Services />
     </main>
   );
 };
